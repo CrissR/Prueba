887a1c378c944f37ac7acddfeb7ed63d38897ef7
Cris <Cris@192.168.1.39>
2014-11-12 11:47:13 +0100

    Prueba

---
+++ HolaMundo1/src/controlador/Saludar.java
@@ -0,0 +1,16 @@
+package controlador;
+
+import modelo.Saluda;
+
+public class Saludar {
+
+	public Saludar() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		Saluda saludo=new Saluda();
+		saludo.diHola("Cristina");
+	}
+
+}
---
+++ HolaMundo1/src/modelo/Saluda.java
@@ -0,0 +1,13 @@
+package modelo;
+
+public class Saluda {
+
+	public Saluda() {
+		
+}
+
+	public void diHola(String nombre) {
+		System.out.println("Hola: "+nombre+" que caña...");		
+	}
+
+}
